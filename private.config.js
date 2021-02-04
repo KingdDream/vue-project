6b90a04fde2e8e7fef534f53eab59d2c506f984a
yangliang <yl18717377230@163.com>
2021-02-04 09:11:40 +0800

    测试

--- private.config.js
+++ private.config.js
@@ -3,12 +3,12 @@
  * @Author                           : yangliang
  * @Date                             : 2020-08-14 17:51:28
  * @LastEditors: yangliang
- * @LastEditTime: 2021-01-22 15:43:46
+ * @LastEditTime: 2021-02-04 09:11:17
  */
 
 // npm run dev
 let sceneArr = {
-  target: '3',                    // 当前模块打包
+  target: '1',                    // 当前模块打包
   0: 'Drag',                      //拖拽        
   1: 'b',
   2: 'c',
